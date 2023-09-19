0ffb61d94396345668be44ae25c14ff3d0a285c0
Seng-Leaph <sengleaphseang@gmail.com>
2023-09-20 02:13:21 +0700

    lotti page

--- lib/cart/order_lottie.dart
+++ lib/cart/order_lottie.dart
@@ -70,119 +70,324 @@ class _LottiState extends State<Lotti> {
                     ),
                   ),
                 ),
-                Container(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 15),
-                          child: Row(
-                            children: [
-                              SizedBox(width: 10,),
-                              Icon(Icons.assignment,color: Colors.pinkAccent,size: 30,),
-                              SizedBox(width: 10,),
-                              Text("Order Summary", style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20
-                              ),),
-                            ],
-                          ),
-                        ),
-                        SizedBox(height: 10,),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 15),
-                          child: Row(
-                            children: [
-                              SizedBox(width: 10,),
-                              Text("1x Spicy Chichen Burgur(N1)",
-                                style: TextStyle(
-                                    fontSize: 18
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 1),
+                  child: Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            child: Row(
+                              children: [
+                                SizedBox(width: 10,),
+                                Text("Order Details", style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20
                                 ),),
-                              Spacer(),
-                              Text('\$1.99',
-                                style: TextStyle(
-                                    fontSize: 18
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 10,),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            child: Row(
+                              children: [
+                                SizedBox(width: 10,),
+                                Text("Your order number: ",
+                                  style: TextStyle(
+                                      fontSize: 18
+                                  ),),
+                                Spacer(),
+                                Text('#s1fu-30ch',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                    color: Colors.blue.shade600,
+                                    backgroundColor: Colors.grey.shade100,
+                                  ),
                                 ),
-                              ),
-                              SizedBox(width: 10,)
-                            ],
+                                SizedBox(width: 10,)
+                              ],
+                            ),
                           ),
-                        ),
-                        Divider(),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 15),
-                          child: Row(
-                            children: [
-                              SizedBox(width: 10,),
-                              Text("Subtotal",
-                                style: TextStyle(
-                                    fontSize: 18
-                                ),),
-                              Spacer(),
-                              Text('\$1.99',
-                                style: TextStyle(
-                                    fontSize: 18
+                          Divider(),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            child: Row(
+                              children: [
+                                SizedBox(width: 10,),
+                                Text("Your order from:",
+                                  style: TextStyle(
+                                      fontSize: 18
+                                  ),),
+                                Spacer(),
+                                Text('KFC Kampuchea Krom',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
-                              ),
-                              SizedBox(width: 10,)
-                            ],
+                                SizedBox(width: 10,)
+                              ],
+                            ),
                           ),
-                        ),
-                        SizedBox(height: 10,),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 15),
-                          child: Row(
-                            children: [
-                              SizedBox(width: 10,),
-                              Text("Delivery",
-                                style: TextStyle(
-                                    fontSize: 18
-                                ),),
-                              Spacer(),
-                              Text('\$1.23',
-                                style: TextStyle(
-                                    fontSize: 18
-                                ),),
-                              SizedBox(width: 10,),
-                            ],
+                          SizedBox(height: 10,),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            child: Row(
+                              children: [
+                                SizedBox(width: 10,),
+                                Text("Delivery Address: ",
+                                  style: TextStyle(
+                                      fontSize: 18
+                                  ),),
+                                Spacer(),
+                                Text('2 St 762, Toul kuk,\n\t\t\t\t\t\t\t\t Phnom Penh ',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                    fontWeight: FontWeight.bold
+                                  ),),
+                                SizedBox(width: 10,),
+                              ],
+                            ),
                           ),
-                        ),
-                        SizedBox(height: 10,),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 15),
-                          child: Row(
-                            children: [
-                              SizedBox(width: 10,),
-                              Text("VAT",
-                                style: TextStyle(
-                                    fontSize: 18
-                                ),),
-                              Spacer(),
-                              Text('\$0.12',
-                                style: TextStyle(
-                                    fontSize: 18
-                                ),),
-                              SizedBox(width: 10,),
-                            ],
+                        ],
+                      ),
+                    ),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 5.0, // soften the shadow
+                          spreadRadius: 3.0, //extend the shadow
+                          offset: Offset(
+                            5.0, // Move to right 5  horizontally
+                            5.0, // Move to bottom 5 Vertically
                           ),
-                        ),
+                        )
                       ],
                     ),
                   ),
-                  decoration: const BoxDecoration(
-                    color: Color(0xFFffffff),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey,
-                        blurRadius: 15.0, // soften the shadow
-                        spreadRadius: 5.0, //extend the shadow
-                        offset: Offset(
-                          5.0, // Move to right 5  horizontally
-                          5.0, // Move to bottom 5 Vertically
-                        ),
-                      )
-                    ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 1),
+                  child: Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            child: Row(
+                              children: [
+                                SizedBox(width: 10,),
+                                Text("1x.\t\t\t Fish chicken",
+                                  style: TextStyle(
+                                      fontSize: 16
+                                  ),),
+                                Spacer(),
+                                Text('\$10.5',
+                                  style: TextStyle(
+                                      fontSize: 16
+                                  ),
+                                ),
+                                SizedBox(width: 10,)
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 10,),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            child: Row(
+                              children: [
+                                SizedBox(width: 10,),
+                                Text("2x. \t\t\t french fried",
+                                  style: TextStyle(
+                                      fontSize: 16
+                                  ),),
+                                Spacer(),
+                                Text('\$13.00',
+                                  style: TextStyle(
+                                      fontSize: 16
+                                  ),),
+                                SizedBox(width: 10,),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 10,),
+                        ],
+                      ),
+                    ),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 5.0, // soften the shadow
+                          spreadRadius: 3.0, //extend the shadow
+                          offset: Offset(
+                            5.0, // Move to right 5  horizontally
+                            5.0, // Move to bottom 5 Vertically
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 1),
+                  child: Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            child: Row(
+                              children: [
+                                SizedBox(width: 10,),
+                                Text("Subtotal", style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20
+                                ),),
+                                Spacer(),
+                                Text('\$25.99',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 10,),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            child: Row(
+                              children: [
+                                SizedBox(width: 10,),
+                                Text("Delivery fee",
+                                  style: TextStyle(
+                                      fontSize: 16
+                                  ),),
+                                Spacer(),
+                                Text('\$0.99',
+                                  style: TextStyle(
+                                      fontSize: 16
+                                  ),
+                                ),
+                                SizedBox(width: 10,)
+                              ],
+                            ),
+                          ),
+                          Divider(),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            child: Row(
+                              children: [
+                                SizedBox(width: 10,),
+                                Text("Incl. Tax",
+                                  style: TextStyle(
+                                      fontSize: 16
+                                  ),),
+                                Spacer(),
+                                Text('\$1.50',
+                                  style: TextStyle(
+                                      fontSize: 16
+                                  ),
+                                ),
+                                SizedBox(width: 10,)
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 10,),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            child: Row(
+                              children: [
+                                SizedBox(width: 10,),
+                                Text("Voucher: aHfuAnh2",
+                                  style: TextStyle(
+                                      fontSize: 16
+                                  ),),
+                                Spacer(),
+                                Text('\$-4.00',
+                                  style: TextStyle(
+                                      fontSize: 16
+                                  ),),
+                                SizedBox(width: 10,),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 10,),
+                        ],
+                      ),
+                    ),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 5.0, // soften the shadow
+                          spreadRadius: 3.0, //extend the shadow
+                          offset: Offset(
+                            5.0, // Move to right 5  horizontally
+                            5.0, // Move to bottom 5 Vertically
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 1),
+                  child: Container(
+                    height: 50,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            child: Row(
+                              children: [
+                                SizedBox(width: 10,),
+                                Text("Total (incl. VAT)", style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20
+                                ),),
+                                Spacer(),
+                                Text('\$8.99',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    decoration: const BoxDecoration(
+                      color: Colors.white70,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 5.0, // soften the shadow
+                          spreadRadius: 3.0, //extend the shadow
+                          offset: Offset(
+                            5.0, // Move to right 5  horizontally
+                            5.0, // Move to bottom 5 Vertically
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ],
